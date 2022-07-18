3687ca90fc610e9a4fe03e1e31fc739a364ff728
prats234 <109065276+prats234@users.noreply.github.com>
2022-07-18 20:36:55 +0530

    Add files via upload

---
+++ operators2.java
@@ -0,0 +1,34 @@
+public class operators2
+{
+    public static void main(String[] args)
+    {
+        //arithmetic operators +,-,*,/
+        float a = 10f;
+        float b = 2f;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println();
+
+        //relational operators <,>,=,==,<=,>=
+
+        System.out.println(a<b);
+        System.out.println(a>b);
+        System.out.println(a==b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+        System.out.println(a!=b);
+        System.out.println();
+
+        //logical operators AND, OR
+        boolean c = true;
+        boolean d = false;
+        System.out.println(c&&d);
+        System.out.println(c||d);
+        System.out.println(c&&c);
+        System.out.println(d||d);
+
+
+    }
+}
